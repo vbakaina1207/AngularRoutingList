4633cd933b5fc4d57f502f7dbe513c1a66b9b211
vbakakina1207 <v_bakakina@ukr.net>
2022-10-02 18:07:17 +0300

    create AngularRoutingList

---
+++ Код з заняття/src/app/admin/admin-discount/admin-discount.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-discount',
+  templateUrl: './admin-discount.component.html',
+  styleUrls: ['./admin-discount.component.scss']
+})
+export class AdminDiscountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
